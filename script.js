e75d3228c436871c5bb4eeae4b4f53dacfb7df0f
Nick <Nick_II2000@mail.ru>
2022-02-11 13:35:27 +0600

    Third commit

--- script.js
+++ script.js
@@ -1,5 +1,46 @@
-const num = new Number(3);
-console.log(num);
+// const num = new Number(3);
+// console.log(num);
 
-const num2 = new Function(3);
-console.log(num2);
+// const num2 = new Function(3);
+// console.log(num2);
+
+/* Функция-конструктор - старый формат*/
+function UserOldSynt(name, id) {
+    this.name = name;
+    this.id = id;
+    this.human = true;
+    this.hello = function () {
+        console.log(`Hello, ${this.name}`);
+    };
+}
+
+UserOldSynt.prototype.exit = function () {
+    console.log(`Пользователь ${this.name} вышел`);
+};
+
+/* Класс - новый формат*/
+class User {
+    constructor(name, id) {
+        this.name = name;
+        this.id = id;
+        this.human = true;
+    }
+    hello() {
+        console.log(`Hello, ${this.name}`);
+    }
+    exit() {
+        console.log(`Пользователь ${this.name} вышел`);
+    }
+}
+
+const ivan = new User('Ivan', 28);
+const alex = new User('Alex', 31);
+
+console.log(ivan);
+console.log(alex);
+
+ivan.hello();
+alex.hello();
+
+ivan.exit();
+alex.exit();
